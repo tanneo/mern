d231fa342f7359a024173c1b2e22227b3635d127
Tegan <teganoar0@gmail.com>
2019-06-20 12:00:15 +1000

    added error 404 for pages not found in app.js

--- app.js
+++ app.js
@@ -13,4 +13,9 @@ app.use(bodyParser.urlencoded({extended: false}))
 app.use(adminRoutes)
 app.use(inviteeRoutes)
 
+app.use((req, res, next) => {
+    res.status(404).send('<h1> Page not found </h1>')
+
+})
+
 app.listen(port, () => console.log(`Server is listening on port ${port}`))
