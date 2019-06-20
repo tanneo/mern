2160c57a06534b000bb446ccf7ed4963d0488797
Tegan <teganoar0@gmail.com>
2019-06-20 11:39:09 +1000

    required and use bodyparser

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require('express')
+const bodyParser = require('body-parser')
 
 const app = express()
 const port = 3000
 
 app.set("view engine", "ejs")
 
+app.use(bodyParser.urlencoded({extended: false}))
 
+app.use('/', function (req, res, next) {
+    res.send('Hello World')
+  });
+  
 
 app.get('/', function (req, res) {
   res.send('Hello World')
